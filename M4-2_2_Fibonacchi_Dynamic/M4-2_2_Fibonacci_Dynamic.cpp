60ce60b7e91fd7a68d61dfab6ab59dc77a0b9007
Mikhail <miktyu88@gmail.com>
2024-02-20 22:23:28 +0300

    Initial commit

---
+++ M4-2_2_Fibonacchi_Dynamic/M4-2_2_Fibonacci_Dynamic.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+long int Fib(int n) {
+    int* fib = new int[n + 2];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[n];
+    delete[] fib;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    do {
+        std::cout << "Введите число: ";
+        std::cin >> n;
+    } while (n < 0);
+    std::cout << "Число Фибоначчи под номером " << n << " равно " << Fib(n) << std::endl;
+    return 0;
+}
